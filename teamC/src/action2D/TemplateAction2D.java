5ea336ac1d63e111a171b8c27bd03554bcbac616
s1671028 <s1671028@PC132G064.ku-kinds.konan-u.ac.jp>
2017-12-15 13:06:41 +0900

    ライフ、アイテムの調整

--- teamC/src/action2D/TemplateAction2D.java
+++ teamC/src/action2D/TemplateAction2D.java
@@ -18,9 +18,13 @@ public class TemplateAction2D extends SimpleActionGame {
 	private Player player;
 	private Enemy enemy_1;
 	private Enemy enemy_2;
+	int invisible=0;//無敵時間
 	//テスト中アイテム変数
 	private Item item;
 	int get_item=1;//アイテム用の関数、取得の際に減少させよう。
+	int Enemy_life_1=1;
+	int Player_life=3;//playerのLIFE
+	int Player_count;
 
 	private Ground2D stage;
 
@@ -63,7 +67,7 @@ public class TemplateAction2D extends SimpleActionGame {
 
 		// 表示範囲を決める（左上が原点としてその原点から幅、高さを計算する）
 		setViewRange(RANGE, RANGE);
-	}
+		}
 
 	@Override
 	public RWTFrame3D createFrame3D() {
@@ -73,7 +77,7 @@ public class TemplateAction2D extends SimpleActionGame {
 		// f.setExtendedState(Frame.MAXIMIZED_BOTH);
 		f.setTitle("Template for Action 2DGame");
 		return f;
-	}
+		}
 
 	@Override
 	public void progress(RWTVirtualController virtualController, long interval) {
@@ -86,11 +90,11 @@ public class TemplateAction2D extends SimpleActionGame {
 		// キー操作の処理
 		// 左
 		if (virtualController.isKeyDown(0, RWTVirtualController.LEFT)) {
-			player.movePositionLeft(0.15);//速度変更
+			player.movePositionLeft(0.1);//速度変更
 		}
 		// 右
 		else if (virtualController.isKeyDown(0, RWTVirtualController.RIGHT)) {
-			player.movePositionRight(0.15);//速度変更
+			player.movePositionRight(0.1);//速度変更
 		}
 		// 上
 		if (virtualController.isKeyDown(0, RWTVirtualController.UP)) {
@@ -119,14 +123,40 @@ public class TemplateAction2D extends SimpleActionGame {
 
 		// 衝突判定（プレイヤーと敵）
 		if (player.checkCollision(enemy_1)) {
-			System.out.println("敵に接触した！");
+			if(Enemy_life_1==1){
+				if(invisible==0){
+				System.out.println("1番の敵に接触した！");
+				Player_life--;
+				System.out.println("現在のライフは"+Player_life+"です");
+				invisible=60;
+				}
+				}
 		}else if(player.checkCollision(enemy_2)) {
-				System.out.println("敵に接触した！");
+			if(invisible==0){
+				System.out.println("2番の敵に接触した！");
+				Player_life--;
+				System.out.println("現在のライフは"+Player_life+"です");
+			invisible=60;
+			}
 			}//BOSSとITEMを追加する
-		else if(player.checkCollision(item)){
+		else if(get_item==1){
+			if(player.checkCollision(item)){
 			System.out.println("アイテムを取得した");
+			Player_life++;
+			System.out.println("現在のライフは"+Player_life+"です");
 			get_item=0;
 			universe.displace(item);
+			}
+		}
+		//敵の消滅判定
+		//if(Enemy_life_1==1){
+			//if(enemy_1.checkCollision(player)){
+				//Enemy_life_1=-1;
+				//universe.displace(enemy_1);
+			//}
+		//}
+		if(invisible!=0){
+			invisible--;
 		}
 	}
 
