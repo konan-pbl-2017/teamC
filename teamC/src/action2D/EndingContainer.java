9ee16e6d9b3a1446e46de87658914290d58ba402
naotan121 <nawoya21@yahoo.co.jp>
2017-12-21 12:24:56 +0900

    スタート、クリア、ゲームオーバー画面の背景を追加・修正しました。

--- teamC/src/action2D/EndingContainer.java
+++ teamC/src/action2D/EndingContainer.java
@@ -20,38 +20,46 @@ public class EndingContainer extends RWTContainer {
 
 		@Override
 		public void build(GraphicsConfiguration gc) {
-		RWTLabel startLabe2 = new RWTLabel();
-		startLabe2.setString("おめでとう！単位を取得できた！");
-		startLabe2.setRelativePosition(0.08f, 0.3f);
-		Font f1 = new Font("", Font.PLAIN, 30);
-		startLabe2.setFont(f1);
-		addWidget(startLabe2);
-		
-		RWTLabel startLabe3 = new RWTLabel();
-		startLabe3.setString("play restart");
-		startLabe3.setRelativePosition(0.70f, 0.85f);
-		Font f2 = new Font("", Font.PLAIN, 20);
-		startLabe3.setFont(f2);
-		addWidget(startLabe3);
-		
-		RWTLabel startLabe4 = new RWTLabel();
-		startLabe4.setString("Press Shiftkey");
-		startLabe4.setRelativePosition(0.68f, 0.95f);
-		startLabe4.setFont(f2);
-		addWidget(startLabe4);
-		   
-		RWTLabel startLabe5 = new RWTLabel();
-		startLabe5.setColor(Color.RED);
-		startLabe5.setString("秀");
-		startLabe5.setRelativePosition(0.4f, 0.75f);
-		Font f5 = new Font("", Font.PLAIN, 80);
-		startLabe5.setFont(f5);
-		addWidget(startLabe5);
-		
-		RWTImage image = new RWTImage("data\\images\\clear.png");
-		addWidgetOnBack(image);
-		
-		repaint();
+			RWTLabel startLabe1 = new RWTLabel();
+			startLabe1.setColor(Color.RED);
+			startLabe1.setString("Game Clear！");
+			startLabe1.setRelativePosition(0.15f, 0.25f);
+			Font f = new Font("", Font.PLAIN, 60);
+			startLabe1.setFont(f);
+			addWidget(startLabe1);
+			
+			RWTLabel startLabe2 = new RWTLabel();
+			startLabe2.setString("おめでとう！単位を取得できた！");
+			startLabe2.setRelativePosition(0.08f, 0.4f);
+			Font f1 = new Font("", Font.PLAIN, 30);
+			startLabe2.setFont(f1);
+			addWidget(startLabe2);
+			
+			RWTLabel startLabe3 = new RWTLabel();
+			startLabe3.setString("play restart");
+			startLabe3.setRelativePosition(0.73f, 0.84f);
+			Font f2 = new Font("", Font.PLAIN, 20);
+			startLabe3.setFont(f2);
+			addWidget(startLabe3);
+			
+			RWTLabel startLabe4 = new RWTLabel();
+			startLabe4.setString("Press Shiftkey");
+			startLabe4.setRelativePosition(0.70f, 0.9f);
+			startLabe4.setFont(f2);
+			addWidget(startLabe4);
+			   
+			RWTLabel startLabe5 = new RWTLabel();
+			startLabe5.setColor(Color.YELLOW);
+			startLabe5.setString("秀");
+			startLabe5.setRelativePosition(0.4f, 0.65f);
+			Font f5 = new Font("", Font.PLAIN, 80);
+			startLabe5.setFont(f5);
+			addWidget(startLabe5);
+			
+			RWTImage image = new RWTImage("data\\images\\teamC\\clear.jpg");
+			addWidgetOnBack(image);
+			
+			repaint();
 	}
 
 	@Override
